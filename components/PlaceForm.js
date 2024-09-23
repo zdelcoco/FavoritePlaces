7cdae487e5214949c65866f9cdf43a43f488798e
Zachary Delcoco <zdelcoco@dmsi.com>
2024-09-23 15:36:14 -0500

    lecture 197 - taking photos on android

--- components/PlaceForm.js
+++ components/PlaceForm.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { View, Text, ScrollView, TextInput, StyleSheet } from 'react-native';
 import { Colors } from '../constants/colors';
+import ImagePicker from './ImagePicker';
 
 function PlaceForm() {
   const [enteredTitle, setEnteredTitle] = useState('');
@@ -19,6 +20,7 @@ function PlaceForm() {
           value={enteredTitle}
         />
       </View>
+      <ImagePicker />
     </ScrollView>
   );
 }
