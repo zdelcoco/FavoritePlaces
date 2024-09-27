67d1cd18810cdc6b3e5bf09f0793b3428cf3e53c
Zachary Delcoco <zdelcoco@dmsi.com>
2024-09-27 09:33:31 -0500

    lecture 212 - outputting a list of places

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,7 +1,20 @@
-import PlacesList from '../components/PlacesList';
+import { useEffect, useState } from 'react';
 
-function AllPlaces() {
-  return <PlacesList />;
+import PlacesList from '../components/PlacesList';
+import { useIsFocused } from '@react-navigation/native';
+
+function AllPlaces({ route }) {
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (isFocused && route.params) {
+      setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+    }
+  }, [route, isFocused]);
+
+  return <PlacesList places={loadedPlaces} />;
 }
 
 export default AllPlaces;
